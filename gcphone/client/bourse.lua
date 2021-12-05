e01f3763fbd3e2f5f66dbbf6643f70f9a420c257
buzelac <95173926+buzelac@users.noreply.github.com>
2021-12-06 01:17:49 +0200

    GCPHONE FOR MUBLEVOİP

---
+++ gcphone/client/bourse.lua
@@ -0,0 +1,25 @@
+--
+--  LEAKED BY S3NTEX -- 
+--  https://discord.gg/aUDWCvM -- 
+--  fivemleak.com -- 
+--  fkn crew -- 
+function setCrypto(mevcut)
+    SendNUIMessage({event = 'updateCrypto', mevcut = mevcut})
+end
+
+RegisterNUICallback('buyCrypto', function(data)
+  TriggerServerEvent('gcPhone:buyCrypto', data.islem, data.id, data.adet, data.fiyat)
+end)
+
+RegisterNUICallback('getCrypto', function(data)
+    ESX.TriggerServerCallback('gcPhone:getCrypto', function(data)
+        setCrypto(data)
+    end, data.id)
+end)
+
+RegisterNetEvent('updateCrypto')
+AddEventHandler('updateCrypto', function(coin)
+    ESX.TriggerServerCallback('gcPhone:getCrypto', function(data)
+        setCrypto(data)
+    end, coin)
+end)
---
+++ gcphone/client/youtube.lua
@@ -0,0 +1,20 @@
+--
+--  LEAKED BY S3NTEX -- 
+--  https://discord.gg/aUDWCvM -- 
+--  fivemleak.com -- 
+--  fkn crew -- 
+RegisterNUICallback('youtube_Play', function(data)
+    exports["xsound"]:Cal(data, false)
+end)
+
+RegisterNUICallback('youtube_Pause', function()
+    exports["xsound"]:Duraklat()
+end)
+
+RegisterNUICallback('youtube_Stop', function() 
+    exports["xsound"]:Durdur()
+end)
+
+RegisterNUICallback('youtube_Resume', function()
+    exports["xsound"]:Devamet()
+end)
